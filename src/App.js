faadb5ace9035c763e6c1ddf7df24b96c991124d
Himanshu Dixit <himanshuharshdixit@gmail.com>
2023-07-21 22:34:37 +0530

    week set dynamically

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import Addweek from "./components/Addweek";
 import Main from "./components/Main";
 
 function App() {
+
+  const [week, setWeek] = useState(false);
+
+  const handleWeek = () => {
+    setWeek(!week);
+  }
+
+
   return (
     <div className="bg-gray-200 h-[100vh]">
-      <Main />
-      {/* <Addweek /> */}
+      <Main handleWeek={handleWeek} week={week} setWeek={setWeek} />
+      <Addweek handleWeek={handleWeek} week={week} setWeek={setWeek} />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import { MdOutlinePostAdd } from 'react-icons/md';
+import data from '../data/week.json'
 
-const Main = () => {
+const Main = (props) => {
+  const Data = data.weeks;
   return (
     <div className='flex flex-col'>
       <div className='flex flex-col md:flex-row justify-between my-4 px-8 py-4 w-[90%] md:w-[80%] m-auto bg-white rounded-xl shadow-lg'>
         <div>
           <ul className='flex justify-center'>
-            <li className='mr-6 hover:bg-gray-600 bg-gray-100 hover:text-white text-gray-500 px-2 md:px-4 py-2 rounded-2xl text-xs md:text-base cursor-pointer'>Week 1</li>
-            <li className='mr-6 hover:bg-gray-600 bg-gray-100 hover:text-white text-gray-500 px-2 md:px-4 py-2 rounded-2xl text-xs md:text-base cursor-pointer'>Week 2</li>
-            <li className='mr-6 hover:bg-gray-600 bg-gray-100 hover:text-white text-gray-500 px-2 md:px-4 py-2 rounded-2xl text-xs md:text-base cursor-pointer'>Week 3</li>
-            <li className='mr-6 hover:bg-gray-600 bg-gray-100 hover:text-white text-gray-500 px-2 md:px-4 py-2 rounded-2xl text-xs md:text-base cursor-pointer'>Week 4</li>
+            {Data.map((item) => {
+              return (
+                <li key={item.id} className='mr-6 hover:bg-gray-600 bg-gray-100 hover:text-white text-gray-500 px-2 md:px-4 py-2 rounded-2xl text-xs md:text-base cursor-pointer'>Week {item.week_number}</li>
+              )
+            })}
           </ul>
         </div>
-        <div className='flex items-center text-gray-500 text-base md:text-lg justify-center cursor-pointer mt-4 md:mt-0'>
+        <div className='flex items-center text-gray-500 text-base md:text-lg justify-center cursor-pointer mt-4 md:mt-0' onClick={() => { props.setWeek(!props.week) }}>
           <div className='mr-1'>
             <MdOutlinePostAdd />
           </div>
